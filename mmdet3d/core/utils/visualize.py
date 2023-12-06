Fixed ground truth rotation.

mmdet3d/core/utils/visualize.py
@@ -9,7 +9,7 @@ from matplotlib import pyplot as plt
 
 from ..bbox import LiDARInstance3DBoxes
 
-__all__ = ["visualize_camera", "visualize_lidar", "visualize_map"]
+__all__ = ["visualize_camera", "visualize_lidar", "visualize_map", "visualize_map_carla"]
 
 
 OBJECT_PALETTE = {
@@ -37,6 +37,13 @@ MAP_PALETTE = {
     "road_divider": (202, 178, 214),
     "lane_divider": (106, 61, 154),
     "divider": (106, 61, 154),
+}
+
+CARLA_MAP_PALETTE = {
+    "road": (128, 64, 128),
+    "car": (0, 0, 142),
+    "truck": (107, 142, 35),
+    "pedestrian": (220, 20, 60)
 }
 
 
@@ -177,6 +184,26 @@ def visualize_map(
     for k, name in enumerate(classes):
         if name in MAP_PALETTE:
             canvas[masks[k], :] = MAP_PALETTE[name]
+    canvas = cv2.cvtColor(canvas, cv2.COLOR_RGB2BGR)
+
+    mmcv.mkdir_or_exist(os.path.dirname(fpath))
+    mmcv.imwrite(canvas, fpath)
+
+def visualize_map_carla(
+    fpath: str,
+    masks: np.ndarray,
+    *,
+    classes: List[str],
+    background: Tuple[int, int, int] = (240, 240, 240),
+) -> None:
+    assert masks.dtype == np.bool, masks.dtype
+
+    canvas = np.zeros((*masks.shape[-2:], 3), dtype=np.uint8)
+    canvas[:] = background
+
+    for k, name in enumerate(classes):
+        if name in CARLA_MAP_PALETTE:
+            canvas[masks[k], :] = CARLA_MAP_PALETTE[name]
     canvas = cv2.cvtColor(canvas, cv2.COLOR_RGB2BGR)
 
     mmcv.mkdir_or_exist(os.path.dirname(fpath))

mmdet3d/datasets/pipelines/loading.py
@@ -323,8 +323,9 @@ class LoadCarlaBEVSegmentation:
         mmcv.check_file_exist(gt_path)
         
         gt_masks = np.load(gt_path)
+        gt_masks = np.rot90(gt_masks, 2, axes=(2, 1))
 
-        data['gt_masks_bev'] = gt_masks
+        data['gt_masks_bev'] = gt_masks.copy()
 
         return data
 

tools/visualize.py
@@ -13,7 +13,7 @@ from torchpack.utils.config import configs
 from tqdm import tqdm
 
 from mmdet3d.core import LiDARInstance3DBoxes
-from mmdet3d.core.utils import visualize_camera, visualize_lidar, visualize_map
+from mmdet3d.core.utils import visualize_camera, visualize_lidar, visualize_map, visualize_map_carla
 from mmdet3d.datasets import build_dataloader, build_dataset
 from mmdet3d.models import build_model
 
@@ -40,9 +40,9 @@ def main() -> None:
 
     parser = argparse.ArgumentParser()
     parser.add_argument("config", metavar="FILE")
-    parser.add_argument("--mode", type=str, default="gt", choices=["gt", "pred"])
+    parser.add_argument("--mode", type=str, default="gt", choices=["gt", "pred", "gt-carla", "pred-carla"])
     parser.add_argument("--checkpoint", type=str, default=None)
-    parser.add_argument("--split", type=str, default="val", choices=["train", "val"])
+    parser.add_argument("--split", type=str, default="val", choices=["train", "val", "test"])
     parser.add_argument("--bbox-classes", nargs="+", type=int, default=None)
     parser.add_argument("--bbox-score", type=float, default=None)
     parser.add_argument("--map-score", type=float, default=0.5)
@@ -68,7 +68,7 @@ def main() -> None:
     )
 
     # build the model and load checkpoint
-    if args.mode == "pred":
+    if args.mode == "pred" or args.mode == "pred-carla":
         model = build_model(cfg.model)
         load_checkpoint(model, args.checkpoint, map_location="cpu")
 
@@ -81,13 +81,13 @@ def main() -> None:
 
     for data in tqdm(dataflow):
         metas = data["metas"].data[0][0]
-        name = "{}-{}".format(metas["timestamp"], metas["token"])
+        name = "{}-SimBEV".format(metas["timestamp"])
 
-        if args.mode == "pred":
+        if args.mode == "pred" or args.mode == "pred-carla":
             with torch.inference_mode():
                 outputs = model(**data)
 
-        if args.mode == "gt" and "gt_bboxes_3d" in data:
+        if (args.mode == "gt" or args.mode == "gt-carla") and "gt_bboxes_3d" in data:
             bboxes = data["gt_bboxes_3d"].data[0][0].tensor.numpy()
             labels = data["gt_labels_3d"].data[0][0].numpy()
 
@@ -121,12 +121,14 @@ def main() -> None:
             bboxes = None
             labels = None
 
-        if args.mode == "gt" and "gt_masks_bev" in data:
+        if (args.mode == "gt" or args.mode == "gt-carla") and "gt_masks_bev" in data:
             masks = data["gt_masks_bev"].data[0].numpy()
-            masks = masks.astype(np.bool)
-        elif args.mode == "pred" and "masks_bev" in outputs[0]:
+            masks = np.rot90(masks, 2, axes=(1, 2))
+            masks = masks.copy().astype(np.bool)
+        elif (args.mode == "pred" or args.mode == "pred-carla") and "masks_bev" in outputs[0]:
             masks = outputs[0]["masks_bev"].numpy()
-            masks = masks >= args.map_score
+            masks = np.rot90(masks, 2, axes=(1, 2))
+            masks = masks.copy() >= args.map_score
         else:
             masks = None
 
@@ -139,7 +141,7 @@ def main() -> None:
                     bboxes=bboxes,
                     labels=labels,
                     transform=metas["lidar2image"][k],
-                    classes=cfg.object_classes,
+                    classes=cfg.classes,
                 )
 
         if "points" in data:
@@ -151,15 +153,22 @@ def main() -> None:
                 labels=labels,
                 xlim=[cfg.point_cloud_range[d] for d in [0, 3]],
                 ylim=[cfg.point_cloud_range[d] for d in [1, 4]],
-                classes=cfg.object_classes,
+                classes=cfg.classes,
             )
 
         if masks is not None:
-            visualize_map(
-                os.path.join(args.out_dir, "map", f"{name}.png"),
-                masks,
-                classes=cfg.map_classes,
-            )
+            if args.mode == "gt" or args.mode == "pred":
+                visualize_map(
+                    os.path.join(args.out_dir, "map", f"{name}.png"),
+                    masks,
+                    classes=cfg.map_classes,
+                )
+            elif args.mode == "gt-carla" or args.mode == "pred-carla":
+                visualize_map_carla(
+                    os.path.join(args.out_dir, "map", f"{name}.png"),
+                    masks,
+                    classes=cfg.classes,
+                )
 
 
 if __name__ == "__main__":